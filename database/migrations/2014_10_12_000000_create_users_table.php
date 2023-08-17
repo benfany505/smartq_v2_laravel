e63782fde37efe249368a7653f93c3db798b1b28
Benfany Aditia <benfany.aditia@gmail.com>
2023-08-17 12:12:25 +0700

    Update 2014_10_12_000000_create_users_table.php
    Update User Table Structure

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,8 +1,10 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -15,20 +17,34 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('user_id')->unique();
-            $table->string('first_name');
-            $table->string('last_name');
+            $table->string('full_name');
             $table->string('username')->unique();
-            $table->string('email')->unique();
             $table->string('role');
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('image_url')->nullable();
             $table->string('created_by');
-            $table->timestamp('last_login_at')->nullable();
-            $table->rememberToken();
+            $table->text('remember_token')->nullable();
+            $table->timestamp('last_login_at')->nullable();            
             $table->timestamps();
         });
+
+        // Insert some stuff
+        DB::table('users')->insert(
+            array(
+                'fullname' => 'Eza Teknologi Nusantara',
+                'username' => 'etn',
+                'role' => 'administrator',
+                'password' => Hash::make('123456'),
+                'image_url' => null,
+                'created_by' => 'admin',
+                'remember_token' => 'admin',
+                'last_login_at' => now(),                
+                'created_at' => now(),
+                'updated_at' => now(),
+            )
+        );
+
+        
     }
 
     /**
